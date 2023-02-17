3383d99940c46ea392b20602241cdef6eeeab731
Azeez Lawal <fatimuh4real@gmail.com>
2023-02-16 22:52:29 -0800

    Dasola is here

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,19 @@
+/*
+ * A C program written by Azeez Lawal
+ * A multibillionaire genius that impart peoples' lives
+ */
+
+#include <stdio.h>
+
+/**
+ * main - using function printf
+ *           to output a statement.
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+
+	return (0);
+}
