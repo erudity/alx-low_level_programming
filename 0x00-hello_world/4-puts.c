8daad5b842b02c3fdaf86e95fa629f9ed60fbb9c
Azeez Lawal <fatimuh4real@gmail.com>
2023-02-16 22:27:41 -0800

    Programming is like building a multilingual puzzle

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,19 @@
+/*
+ * A first C program written by Azeez Lawal
+ * A multibillionaire genius that impart peoples' lives
+ */
+
+#include <stdio.h>
+
+/**
+ * main - using function puts
+ *           to output a statement.
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
