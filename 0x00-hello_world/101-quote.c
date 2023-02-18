d175a0f73152f184ac40c4e031a95c71d0f01ba8
Azeez Lawal <fatimuh4real@gmail.com>
2023-02-17 16:08:13 -0800

    to output a standard error.

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,21 @@
+/*
+ * A C program written by Azeez Lawal
+ * A multibillionaire genius that impart peoples' lives
+ */
+
+#include <unistd.h>
+
+/**
+ * main - using function write(2)
+ *           to output a standard error.
+ *
+ * Return: always 1 (success)
+ */
+int main(void)
+{
+	write(2,
+	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	      59);
+
+	return (1);
+}
