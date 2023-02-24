78c5843c5a9d6a2c1e3436ce4ad8651755a60bb7
Azeez Lawal <fatimuh4real@gmail.com>
2023-02-24 02:38:52 -0800

    Pedantic must come before Werror for compilation

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,17 +1,4 @@
-#include <unistd.h>
 #include "main.h"
-
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
 
 /**
  * main - Entry Point
@@ -21,14 +8,14 @@ int _putchar(char c)
 int main(void)
 {
 	/*This used the ASCII code to print out '_putchar'*/
-	_putchar(95);
-	_putchar(112);
-	_putchar(117);
-	_putchar(116);
-	_putchar(99);
-	_putchar(104);
-	_putchar(97);
-	_putchar(114);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 
 	return (0);
