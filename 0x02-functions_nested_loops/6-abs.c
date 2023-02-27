b78dac8e9595cd0b18ca7b48e5c5a9b0faa0d4c0
Azeez Lawal <fatimuh4real@gmail.com>
2023-02-26 18:09:26 -0800

    What an absolute nonsense sign

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _abs - a function that computes the absolute value of an integer.
+ *
+ * @a: a positive or negative number
+ *
+ * Return: a positive number  or zero in all cases
+ */
+int _abs(int a)
+{
+	if (a <= 0)
+	{
+		a = -a;
+		return (a);
+	}
+	else
+	{
+		return (a);
+	}
+}
